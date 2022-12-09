75cfaf00a24a45d8bbd9f2ef8d5fffd80e61c696
gasperpb <achillespb@hotmail.com>
2022-12-09 15:35:11 -0300

    antes de instalar o tailwind

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,10 +2,22 @@ import { ShoppingCart } from 'phosphor-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "../components/navbar.css"
+import addmobile from "../assets/addmobile.webp"
+import addweb from "../assets/addweb.webp"
+
 
 export const Navbar = () => {
     return (
-        <div className='navbar'>
+        <div className=" bg-black flex justify-center">
+            <div className="flex sm:hidden mx-auto">
+                <img src={addmobile} alt="" />
+            </div>
+            <div className="hidden sm:flex mx-auto">
+                <img src={addweb} alt="" />
+
+                <div className='navbar'>
+                </div>
+            </div>
             <div className='links'>
                 <Link to="/">Shop</Link>
                 <Link to="/cart"><ShoppingCart size={32} /></Link>
